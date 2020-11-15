2e60a2a11cac83dff4cb2ea434cf5d6ba43162c3
AbhipsaGuru1012 <58511560+AbhipsaGuru1012@users.noreply.github.com>
2020-11-15 17:21:41 +0530

    initial commit

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const weatherRoute=require("./routes/weather")
 
 app.set('view engine', 'ejs');
 
-//Middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 
-//Middleware route
 app.use('/', weatherRoute);
 
 const port=process.env.PORT || 3000;
